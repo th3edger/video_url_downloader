1d9e1df7807acb057f12a9d7be18c97d7a17c4d1
edgarperez97 <edgarperez_97@hotmail.com>
2021-09-16 17:11:23 -0500

    se modifican los archivos .gitignore y scraper.py ademas de eliminar xpath.txt y se añade el README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Video URL Downloader
+
+## Introducción
+
+El siguiente script sirve para extraer de manera automatica los links para descargar posteriormente los episodios de anime de la pagina de <https://www.Japan-paw.net>
+
+
+
+## Instalación
+
+> **Note**: Para ejecutar con éxito el programa, se tiene que ejecutar el siguiente comando para instalar las dependencias necesarias:
+
+
+```sh
+pip3 install -r requirements.txt 
+```
--- scraper.py
+++ scraper.py
@@ -4,34 +4,62 @@ import lxml.html as html
 # LINK = 'https://biblioteca.japan-paw.wtf/0:/Anime/B/[Tsubaki]%20Bokura%20wa%20Minna%20Kawai-sou%20[BD%201080p]/[Tsubaki]%20Bokura%20wa%20Minna%20Kawai-sou%20[BD%201080p]mirror.html'
 
 XPATH_LINKS_A_DESCARGAR = '//td/a[@class = "enlaces"]/@href'
-XPATH_TITULO = '//td/a[@class = "enlaces" and @href]/p/text()'
+XPATH_TITULOS_CAPS = '//td/a[@class = "enlaces" and @href]/p/text()'
+XPATH_SERIE = '//div[@style]/h1/text()'
+
+
+def make_files(links, t_caps, nombre_anime):
+    with open('zeldas.txt', 'w') as f:
+        for iterador in links:
+            f.write(iterador)
+
+    diccionario = dict(zip(t_caps, links))
+
+    with open('diccionario.txt', 'w') as f:
+
+        f.write(f"{nombre_anime} \n\n")
+
+        for llave in diccionario:
+            f.write(f"{llave} : {diccionario[llave]}" )
+
+
 
 def parse_links(link_a_scrapear):
     try:
-        # response = requests.get(LINK)
-        response = requests.get(link_a_scrapear) 
         
-        if response.status_code == 200:
+        respuesta = requests.get(link_a_scrapear)
+        respuesta2 = requests.get(link_a_scrapear)
+        respuesta3 = requests.get(link_a_scrapear)
+        
+        if respuesta.status_code and respuesta2.status_code and respuesta3.status_code == 200:
             
-            link = response.content.decode('utf-8')
+            link = respuesta.content.decode('utf-8')
+            link_2 = respuesta2.content.decode('utf-8')
+            link_3 = respuesta3.content.decode('utf-8')
+
             parseado = html.fromstring(link)
+            parseado2 = html.fromstring(link_2)
+            parseado3 = html.fromstring(link_3)
 
             links_a_descargar = parseado.xpath(XPATH_LINKS_A_DESCARGAR)
+            titulos_caps = parseado2.xpath(XPATH_TITULOS_CAPS)
+            nombre_anime = parseado3.xpath(XPATH_SERIE)[0]
+            nombre_anime = nombre_anime.replace('\'', '')
 
-            for i in links_a_descargar:
-                print(i)
+            make_files(links_a_descargar, titulos_caps, nombre_anime)
 
         else:
-            raise ValueError(f'Error: {response.status_code}')
+            raise ValueError(f'Error: {respuesta.status_code}')
 
     except ValueError as ve:
         print(ve)
 
 
+
 def run():
 
     pagina = input("Ingrese el link de la descarga:  ")
-
+    print()
     parse_links(pagina)
 
 
