7df76603581a58cc2b974fa53bb0f98362afbf44
Edgar Alberto <edgarperez_97@hotmail.com>
2021-12-21 17:18:57 -0600

    se termino jp_page_object

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import argparse
+import re
 import logging
-import jp_page_object as jp
 
+import jp_page_object as jp
 from common import config
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
+enlace_valido = re.compile(r'https?://biblioteca.+\.m[kp]{1,1}[v4]{1,1}')
 
 
 def _series_scraper(serie_uid):
@@ -16,13 +17,21 @@ def _series_scraper(serie_uid):
     
     jp_pagina_serie = jp.JpSeriePage(serie_uid, host)
     
+    links_bien_chiditos = []
     for link in jp_pagina_serie.link_caps:
-        print(link)
+        link = _fetch_link(link)
+        links_bien_chiditos.append(link)
     
-    for nombre in jp_pagina_serie.nom_caps:
-        print(nombre)
 
     print(f'\n\n{jp_pagina_serie.titulo_serie}\n\n')
+    for nom_cap, link_cap in zip(jp_pagina_serie.nom_caps, links_bien_chiditos):
+        print(f'{nom_cap}:\t{link_cap}')
+
+
+def _fetch_link(link_a_revisar):
+    link = re.findall(pattern=enlace_valido, string=link_a_revisar)
+    string_link = ''.join(link)
+    return string_link
 
 
 
