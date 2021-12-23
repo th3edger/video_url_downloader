630a23635b26d6b8cd9ffe93aa1da3f7ba575521
Edgar Alberto <edgarperez_97@hotmail.com>
2021-12-23 03:43:40 -0600

    se modifico common.py para agregar funciones del manejo del Ctrl. + C y visualizacion de tiempo de ejecucion, asi como el cambio de nombre de receipe.py a transform/main.py

--- extract/common.py
+++ extract/common.py
@@ -1,3 +1,6 @@
+import signal
+import sys
+import time
 import yaml
 
 __config = None
@@ -9,3 +12,24 @@ def config():
             __config = yaml.safe_load(file)
     
     return __config
+
+
+##MEDIR TIEMPO
+def tiempo_de_ejecucion(func):
+    def envoltura(*args, **kwargs):
+        tiempo_inicial = time.time()
+        func(*args, **kwargs)
+        tiempo_final = time.time()
+        tiempo_transcurrido = tiempo_final-tiempo_inicial
+        tiempo_transcurrido = round(tiempo_transcurrido, 3)
+        print(f"Transcurrieron: {tiempo_transcurrido} segundos")
+
+    return envoltura
+
+
+#MANEJO DEL Ctrl. + C
+def def_handler(sig, frame):
+    print("\nSaliendo...\n")
+    sys.exit()
+
+signal.signal(signal.SIGINT, def_handler)
---
+++ transform/main.py
@@ -0,0 +1,65 @@
+import argparse
+import hashlib
+import logging
+
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main(archivo):
+    logger.info('Empezando la modificacion del archivo')
+    
+    df = _read_data(archivo)
+    df = _generate_uids_for_rows(df)
+    df = _drops_rows_with_missing_values(df)
+    _save_data(df, archivo)
+
+    return df
+
+
+##Funciones para Main
+def _read_data(archivo):
+    logger.info('Leyendo el Archivo CSV')
+    return pd.read_csv(archivo)
+
+
+def _generate_uids_for_rows(df):
+    logger.info('Generando uids para cada fila')    
+    
+    uids = (
+        df
+        .apply( lambda fila: hashlib.md5(bytes(fila['url'].encode())), axis=1 )
+        .apply( lambda hashobejct: hashobejct.hexdigest() )
+    )
+
+    df['uid'] = uids
+    df.set_index('uid', inplace=True)
+
+    return df
+
+
+def _drops_rows_with_missing_values(df):
+    logger.info('Eliminando las filas en donde no hay valores')
+    return df.dropna()
+
+def _save_data(df, filename):
+    logger.info(f'Guardando la informacion en {filename}')
+    df.to_csv(f'nuevo_{filename}')
+
+
+
+##
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'archivo',
+        help='Archivo con insformacion a transformar',
+        type=str
+    )
+
+    argumento = parser.parse_args()
+
+    df = main(argumento.archivo)
+    print(df)
